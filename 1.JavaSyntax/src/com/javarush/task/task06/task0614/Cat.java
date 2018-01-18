e361f57ca15ac51d5d143890240a8e002338a64e
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-01-18 09:49:25 +0300

    task 0611-0622,0701  complete

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task06.task0614;
+
+import java.util.ArrayList;
+
+/* 
+Статические коты
+*/
+
+public class Cat {
+    //напишите тут ваш код
+
+    public static ArrayList<Cat> cats = new ArrayList<>();
+
+
+
+    public Cat() {
+        cats.add(this);
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        //Cat.cats = new ArrayList<>();
+        for (int i =0; i<10; i++) {
+            Cat.cats.add(new Cat());
+        }
+        printCats();
+    }
+
+    public static void printCats() {
+        //напишите тут ваш код
+
+        for(Cat cat:cats) {
+            System.out.println(cat);
+        }
+    }
+}
