1eed68a97b3128c92da2afe59f4a14006d6e9c47
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-02-13 23:05:59 +0300

    task 1201 - 10 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        Integer b = 5;
+        print(a);
+        print(b);
+    }
+    //Напишите тут ваши методы
+    private static void print(Integer a) {
+        System.out.println(a);
+    }
+    private static void print(int a) {
+        System.out.println(a);
+    }
+}
