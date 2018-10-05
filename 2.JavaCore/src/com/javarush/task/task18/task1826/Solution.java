c65d731efb5046694726741d3ea57235142e5868
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-10-05 09:19:23 +0300

    task1826 in progress

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -4,9 +4,41 @@ package com.javarush.task.task18.task1826;
 Шифровка
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+
+    private static void encodeFile(String fileName, String fileOutputName) {
+        //method for encode file
+    }
+
+    private static void decodeFile() {
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        String fileName = args[1];
+        String fileOutputName = args[2];
+
+        //File fileIn = new File(args[1]);
+        //File fileOut = new File(args[2]);
+
+        FileInputStream fis = new FileInputStream(args[1]);
+        FileOutputStream fos = new FileOutputStream(args[2]);
+
+        //int b = fis.read();
+
+        while (fis.available() > 0) {
+            int b = fis.read();
+            fos.write(b+1);
+        }
+
+
+        System.out.println(args[0]);
         System.out.println(args[1]);
+        System.out.println(args[2]);
     }
 
 }
