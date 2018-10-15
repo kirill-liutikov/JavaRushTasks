4443359d64f411fcaedc805e756a6b16d29d9187
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-10-15 13:20:09 +0300

    task 1823-1824 complete

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -19,6 +19,51 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        IncomeData data = new IncomeData() {
+            @Override
+            public String getCountryCode() {
+                return "UA";
+            }
+
+            @Override
+            public String getCompany() {
+                return "JavaRush Ltd.";
+            }
+
+            @Override
+            public String getContactFirstName() {
+                return "Ivan";
+            }
+
+            @Override
+            public String getContactLastName() {
+                return "Ivanov";
+            }
+
+            @Override
+            public int getCountryPhoneCode() {
+                return 38;
+            }
+
+            @Override
+            public int getPhoneNumber() {
+                return 234567;
+            }
+        };
+
+        IncomeDataAdapter adapter = new IncomeDataAdapter(data);
+
+        System.out.println(adapter.getPhoneNumber());
+
+        //Customer customer = new IncomeDataAdapter(data);
+
+        //Contact contact = new IncomeDataAdapter(data);
+
+        //System.out.println(customer.getCompanyName());
+        //System.out.println(customer.getCountryName());
+
+
+
     }
 
     public static class IncomeDataAdapter implements Customer, Contact{
@@ -46,14 +91,16 @@ public class Solution {
 
         @Override
         public String getPhoneNumber() {
-            String code = data.getCountryCode();
-            String localPhone = String.valueOf(data.getPhoneNumber());
-            String localPhoneForm = ("0000000000"+localPhone).substring(localPhone.length());
-            String phone = "+" + code + localPhone;
-            //String getPhone =
+            //String code = String.valueOf(data.getCountryPhoneCode());
+            //String localPhone = String.valueOf(data.getPhoneNumber());
+            //String localPhoneForm = ("0000000000"+localPhone).substring(localPhone.length());
+            //String phone = "+" + code + localPhoneForm;
+            //System.out.println(phone);
+            //String formatPhone = phone.replaceFirst("(\\d{2})(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "$1($2)$3-$4-$5");
+            String phone = String.format("%010d",data.getPhoneNumber());
+            System.out.println(phone);
 
-
-            return null;
+            return String.format("+%d(%s)%s-%s-%s", data.getCountryPhoneCode(), phone.substring(0, 3), phone.substring(3, 6),phone.substring(6, 8), phone.substring(8, 10));
         }
     }
 
