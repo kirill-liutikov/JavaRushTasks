fc5edbed058400c81b5b4552c07d4e811390b04f
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-10-17 08:21:14 +0300

    1905 complete

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -10,14 +10,51 @@ import java.util.Map;
 public class Solution {
     public static Map<String,String> countries = new HashMap<String,String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
+        Customer customer = new Customer() {
+            @Override
+            public String getCompanyName() {
+                return "JavaRush Ltd.";
+            }
+
+            @Override
+            public String getCountryName() {
+                return "Ukraine";
+            }
+        };
+
+        Contact contact = new Contact() {
+            @Override
+            public String getName() {
+                return "Ivanov, Ivan";
+            }
+
+            @Override
+            public String getPhoneNumber() {
+                return "+38(050)123-45-67";
+            }
+        };
+
+        DataAdapter dataAdapter = new DataAdapter(customer, contact);
+
+        System.out.println(dataAdapter.getCountryCode());
+        System.out.println(dataAdapter.getContactFirstName());
+        System.out.println(dataAdapter.getContactLastName());
+        System.out.println(dataAdapter.getDialString());
+
 
     }
 
     public static class DataAdapter implements RowItem{
 
-        Customer customer;
-        Contact contact;
+        private Customer customer;
+        private Contact contact;
 
         public DataAdapter(Customer customer, Contact contact) {
             this.customer = customer;
@@ -26,27 +63,35 @@ public class Solution {
 
         @Override
         public String getCountryCode() {
-            return null;
+            String countryCode = null;
+            for (Map.Entry entry : countries.entrySet()) {
+                if (entry.getValue().equals(customer.getCountryName())) {
+                    countryCode = entry.getKey().toString();
+                }
+            }
+            return countryCode;
         }
 
         @Override
         public String getCompany() {
-            return null;
+            return customer.getCompanyName();
         }
 
         @Override
         public String getContactFirstName() {
-            return null;
+            String[] nameFirstLast = contact.getName().split(", ");
+            return nameFirstLast[1];
         }
 
         @Override
         public String getContactLastName() {
-            return null;
+            String[] nameFirstLast = contact.getName().split(", ");
+            return nameFirstLast[0];
         }
 
         @Override
         public String getDialString() {
-            return null;
+            return "callto://" + contact.getPhoneNumber().replace("(","" ).replace(")", "").replace("-","");
         }
     }
 
