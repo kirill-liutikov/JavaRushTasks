1095f26e84aa1b924f130aaee169420331c89817
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-10-19 17:36:44 +0300

    task 1915 complete

--- 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -4,10 +4,40 @@ package com.javarush.task.task19.task1915;
 Дублируем текст
 */
 
+import java.io.*;
+
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName1 = reader.readLine();
+
+        FileOutputStream fos = new FileOutputStream(fileName1);
+
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        String line = byteArrayOutputStream.toString();
+
+        fos.write(byteArrayOutputStream.toByteArray());
+
+        System.setOut(consoleStream);
+
+        fos.close();
+        reader.close();
+        System.out.println(line);
+
+
+
     }
 
     public static class TestString {
