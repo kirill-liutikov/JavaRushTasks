669de47f88be56505a876afdd024611b0a3930a8
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-10-19 08:40:03 +0300

    1913-1914 complete

--- 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -4,13 +4,30 @@ package com.javarush.task.task19.task1913;
 Выводим только цифры
 */
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
         PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        String line = byteArrayOutputStream.toString().replaceAll("[^\\d+]", "");
+
+        System.setOut(consoleStream);
+
+        System.out.println(line);
     }
 
     public static class TestString {
