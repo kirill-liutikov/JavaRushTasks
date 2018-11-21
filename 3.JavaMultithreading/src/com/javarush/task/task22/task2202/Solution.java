4ebc360b2261ca18868aa21f702094288a8c4f6f
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-11-21 21:21:38 +0300

    Task 2202 complete

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -12,18 +12,17 @@ public class Solution {
 
         if (string == null) throw new TooShortStringException();
 
-        //System.out.println(string.split(" ").length);
 
         if (string.split(" ").length < 5) throw new TooShortStringException();
 
         int firstSpace = string.indexOf(" ");
-        int secondSpace = string.indexOf(" ",15);
-        int tirthSpace = string.indexOf(" ",secondSpace);
-        int fourSpace = string.indexOf(" ",tirthSpace);
+        int secondSpace = string.indexOf(" ",firstSpace+1);
+        int thirthSpace = string.indexOf(" ",secondSpace+1);
+        int fourSpace = string.indexOf(" ",thirthSpace+1);
+        int fiveSpace = string.indexOf(" ",fourSpace+1);
 
-        System.out.println(fourSpace);
-
-        return string.substring(firstSpace+1, fourSpace);
+        if (fiveSpace == -1) return string.substring(firstSpace+1);
+        return string.substring(firstSpace+1, fiveSpace);
     }
 
     public static class TooShortStringException extends RuntimeException {
