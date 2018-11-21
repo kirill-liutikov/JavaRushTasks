41c155d51b6e6203fe544389c17a2b763ad33f6d
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-11-21 21:27:18 +0300

    Task 2202 refactor

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -4,27 +4,18 @@ package com.javarush.task.task22.task2202;
 Найти подстроку
 */
 public class Solution {
-    public static void main(String[] args) throws TooShortStringException {
+    public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
     }
 
-    public static String getPartOfString(String string) throws TooShortStringException {
-
+    public static String getPartOfString(String string) {
         if (string == null) throw new TooShortStringException();
+        if (string.split(" ").length < 5) throw  new TooShortStringException();
 
-
-        if (string.split(" ").length < 5) throw new TooShortStringException();
-
-        int firstSpace = string.indexOf(" ");
-        int secondSpace = string.indexOf(" ",firstSpace+1);
-        int thirthSpace = string.indexOf(" ",secondSpace+1);
-        int fourSpace = string.indexOf(" ",thirthSpace+1);
-        int fiveSpace = string.indexOf(" ",fourSpace+1);
-
-        if (fiveSpace == -1) return string.substring(firstSpace+1);
-        return string.substring(firstSpace+1, fiveSpace);
+        String[] str = string.split(" ");
+        return String.format("%s %s %s %s", str[1], str[2], str[3], str[4]);
     }
 
-    public static class TooShortStringException extends RuntimeException {
+    public static class TooShortStringException extends RuntimeException{
     }
 }
