c25ef2ff40f62d5b0141a9fb9d286737070a8f44
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-11-28 16:59:17 +0300

    task 2301-2312 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+
+        Solution sol1 = new Solution();
+        Solution sol2 = new Solution();
+
+        sol1.innerClasses[0] = sol1.new InnerClass();
+        sol1.innerClasses[1] = sol1.new InnerClass();
+
+        sol2.innerClasses[0] = sol2.new InnerClass();
+        sol2.innerClasses[1] = sol2.new InnerClass();
+
+        Solution[] solutions = new Solution[2];
+        solutions[0] = sol1;
+        solutions[1] = sol2;
+
+        return solutions;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
