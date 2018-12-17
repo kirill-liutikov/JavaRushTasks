ba08922b96a57b5a42f8f919e25eba5b42d4921e
Kirill Liutikov <kirill.liutikov@gmail.com>
2018-12-17 08:31:54 +0300

    Task 3608 in progress

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Subject.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task23.task2309.vo;
+
+/**
+ * Created by kirill on 13.12.2018.
+ */
+public class Subject extends NamedItem {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.Arrays;
+
+/**
+ * Created by kirill on 16.12.2018.
+ */
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(Arrays.asList(new User("First", 1,1), new User("Second", 2,2)));
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        throw new UnsupportedOperationException();
+    }
+}
