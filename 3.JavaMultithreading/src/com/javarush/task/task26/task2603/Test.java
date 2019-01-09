6688c14b2f7d50b8dc4cd791f4e02a83ed624756
Kirill Liutikov <kirill.liutikov@gmail.com>
2019-01-09 19:51:07 +0300

    task 2603 in progress

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Test.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Test.java
@@ -1,54 +1,114 @@
 package com.javarush.task.task26.task2603;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * Created by карамба on 09.01.2019.
  */
-public class Test implements Comparable{
-    private String firstName;
-    private String secondName;
-    private int age;
 
-    public String getFirstName() {
-        return firstName;
+class Product {
+   private String name;
+   private double price;
+   private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getName() {
+        return name;
     }
 
-    public String getSecondName() {
-        return secondName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
+    public double getPrice() {
+        return price;
     }
 
-    public int getAge() {
-        return age;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     @Override
-    public int compareTo(Object o) {
-        return this.getFirstName().compareTo(((Test) o).getFirstName());
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+
+class SortedByName implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        String s1 = o1.getName();
+        String s2 = o1.getName();
+
+        return s1.compareTo(s2);
+    }
+}
+
+class SortedByPrice implements Comparator<Product> {
+
+    @Override
+    public int compare(Product o1, Product o2) {
+        Double d1 = o1.getPrice();
+        Double d2 = o2.getPrice();
+        return d1.compareTo(d2);
+    }
+}
+
+class CustomizedComparator<T> implements Comparator<T> {
+
+    private Comparator<T>[] comparators;
+
+    public CustomizedComparator(Comparator... comparators) {
+        this.comparators = comparators;
     }
 
-
-    public class testSortByFirstName implements Comparator {
-
-        @Override
-        public int compare(Object o1, Object o2) {
-            return 0;
+    @Override
+    public int compare(T o1, T o2) {
+        int result =0;
+        for (Comparator comparator : comparators) {
+            result = comparator.compare(o1,o2);
+            if (result != 0) {
+            break;}
         }
+        return result;
     }
+}
+
+public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Product[] prodArray = new Product[3];
+
+        prodArray[0] = new Product("BMilk", 7.56, 56);
+
+        prodArray[1] = new Product("AMilk", 17.00, 32);
+
+        prodArray[2] = new Product("BTea", 12.50, 0);
+
+        CustomizedComparator comparator = new CustomizedComparator<>(new SortedByName(), new SortedByPrice());
+
+        Arrays.sort(prodArray, comparator);
+
+        Arrays.stream(prodArray).forEach(x -> System.out.println(x));
+
+
     }
 }
